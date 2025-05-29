131aa411538c2c18a548ddc63abf261b39108a52
CallMeYurii <vad4807@gmail.com>
2025-05-29 10:07:26 +0300

    fix issue with logger injection
    specify the object type for logger

--- src/Services/Catalog/Catalog.API/Features/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Features/GetProducts/GetProductsHandler.cs
@@ -9,7 +9,7 @@ public record GetProductsResult(
     IEnumerable<Product> Products
 );
 
-public class GetProductsQueryHandler(IQuerySession session, ILogger logger)
+public class GetProductsQueryHandler(IQuerySession session, ILogger<GetProductsQueryHandler> logger)
     : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
