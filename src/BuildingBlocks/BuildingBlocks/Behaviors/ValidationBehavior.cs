63573b8382d373f36b71b1289529d8c21fed9465
CallMeYurii <vad4807@gmail.com>
2025-06-23 12:19:57 +0300

    Provide generic fluent validator for mediatr pipeline behavior

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+using BuildingBlocks.CQRS;
+using ValidationException = FluentValidation.ValidationException;
+
+namespace BuildingBlocks.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ICommand<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResults =
+            await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+
+        var errors =
+            validationResults
+                .Where(vr => vr.Errors.Any())
+                .SelectMany(vr => vr.Errors)
+                .ToList();
+
+        if (errors.Any())
+        {
+            throw new ValidationException($"{errors.FirstOrDefault()}");
+        }
+
+        return await next(cancellationToken);
+    }
+}
