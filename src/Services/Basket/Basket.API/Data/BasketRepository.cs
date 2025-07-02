b4b24f197e0b1205c4e96c6113809dc386ad228a
CallMeYurii <vad4807@gmail.com>
2025-07-02 09:53:03 +0300

    Add repository pattern

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,26 @@
+﻿using Basket.API.Exceptions;
+
+namespace Basket.API.Data;
+
+public class BasketRepository(IQuerySession qSession, IDocumentSession dSession) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        var basket = await qSession.LoadAsync<ShoppingCart>(userName, cancellationToken);
+        return basket ?? throw new BasketNotFoundException(userName);
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        dSession.Store(basket);
+        await dSession.SaveChangesAsync(cancellationToken);
+        return basket;
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        dSession.Delete<ShoppingCart>(userName);
+        await dSession.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,4 @@
-using BuildingBlocks.Behaviors;
-
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 
@@ -8,12 +6,21 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
 builder.Services.AddCarter();
 
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opt.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 // Configure pipeline
+
+app.MapCarter();
 
 app.Run();
